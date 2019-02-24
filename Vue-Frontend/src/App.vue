e04471ed289bb284488943fb0d82a97633dbb37d
Damith Gunner <djnitehawk83@gmail.com>
2019-02-24 20:10:18 +0530

    initial commit

---
+++ Vue-Frontend/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-app>
+    <v-toolbar flat>
+      <!-- <v-toolbar-side-icon ></v-toolbar-side-icon> -->
+      <v-toolbar-title>
+        <span class="pa-2 white--text font-weight-bold display-3 black">KiwiLink</span>
+        <span class="display-2">CRM</span>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-toolbar-items class="hidden-sm-and-down">
+        <v-btn flat>Link One</v-btn>
+        <v-btn flat>Link Two</v-btn>
+        <v-btn flat>Link Three</v-btn>
+      </v-toolbar-items>
+    </v-toolbar>
+
+    <v-content>
+      <router-view />
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: "App",
+  data() {
+    return {};
+  }
+};
+</script>
