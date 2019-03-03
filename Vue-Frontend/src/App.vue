f47410db721220b30156ac96344bde2f04afc235
Damith Gunner <djnitehawk83@gmail.com>
2019-03-03 09:43:40 +0530

    ready for development

--- Vue-Frontend/src/App.vue
+++ Vue-Frontend/src/App.vue
@@ -1,25 +1,7 @@
 <template>
   <v-app>
-    <v-toolbar
-      flat
-      color="indigo lighten-1"
-      class="py-1"
-    >
-      <!-- <v-toolbar-side-icon ></v-toolbar-side-icon> -->
-      <v-toolbar-title>
-        <span class="px-2 white--text font-weight-bold display-3 black">KiwiLink</span>
-        <span class="display-1">CRM</span>
-      </v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-toolbar-items class="hidden-sm-and-down">
-        <v-btn flat>Link One</v-btn>
-        <v-btn flat>Link Two</v-btn>
-        <v-btn flat>Link Three</v-btn>
-      </v-toolbar-items>
-    </v-toolbar>
-
     <v-content>
-      <router-view />
+      hellow
     </v-content>
   </v-app>
 </template>
@@ -27,6 +9,7 @@
 <script>
 export default {
   name: "App",
+  components: {},
   data() {
     return {};
   }
--- Vue-Frontend/src/main.js
+++ Vue-Frontend/src/main.js
@@ -1,13 +1,9 @@
 import Vue from "vue";
-import "./vuetify";
+import "./plugins/vuetify";
 import App from "./App.vue";
-import router from "./router";
-import "roboto-fontface/css/roboto/roboto-fontface.css";
-import "@fortawesome/fontawesome-free/css/all.css";
 
 Vue.config.productionTip = false;
 
 new Vue({
-  router,
   render: h => h(App)
 }).$mount("#app");
